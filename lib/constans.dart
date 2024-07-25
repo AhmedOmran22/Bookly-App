34b5c88b5eb5e6fa743d9de66e92cbfbbc4c151b
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-07-25 16:44:15 +0300

    refactor splash view body and navigate to home view

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color kBackgroundColor = Color(0xff100E23);
- 
+const kDurationTransition = Duration(milliseconds: 1200);
---
+++ lib/features/splash feature/presintation/views/widgets/splash_view_body.dart
@@ -0,0 +1,81 @@
+import 'package:bookly/constans.dart';
+import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/home/presintation/view/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'sliding_text.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimaion;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigatoToHome();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+        ),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimaion: slidingAnimaion),
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    slidingAnimaion = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  Future<dynamic> navigatoToHome() {
+    return Future.delayed(
+      const Duration(seconds: 3),
+      () => Get.off(
+        const HomeView(),
+        transition: Transition.native,
+        duration: kDurationTransition,
+      ),
+    );
+  }
+}
+
+
+   // Navigator.of(context).pushReplacement(
+        //   MaterialPageRoute(
+        //     builder: (context) {
+        //       return const HomeView();
+        //     },
+        //   ),
+        // );

f94e06b7736a4cdf33008068c877ea5cf60c9ec6
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-07-25 12:25:40 +0300

    splash view done before adding animation to it

---
+++ lib/features/splash feature/presintation/widgets/splash_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/constans.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'features/splash feature/presintation/views/splash_view.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -10,9 +13,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp
-    (
-      
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kBackgroundColor,
+      ),
+      home: const SplashView(),
     );
   }
 }

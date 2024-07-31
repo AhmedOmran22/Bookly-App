ad05fb99338fa372e8b4b668e88d317561cde25b
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-07-31 18:04:03 +0300

    create simillar books list view in book detailes view

--- lib/features/book feature/presintation/view/widgets/book_detailes_view_body.dart
+++ lib/features/book feature/presintation/view/widgets/book_detailes_view_body.dart
@@ -4,6 +4,7 @@ import 'book_rating.dart';
 import 'books_actions.dart';
 import 'custom_book_detailes_app_bar.dart';
 import 'custom_book_image.dart';
+import 'simillar_books_list_view.dart';
 
 class BookDetailesViewBody extends StatelessWidget {
   const BookDetailesViewBody({super.key});
@@ -19,27 +20,41 @@ class BookDetailesViewBody extends StatelessWidget {
           child: Column(
             children: [
               const CustomAppBarBookDerailes(),
-              const SizedBox(height: 35),
+              const SizedBox(height: 30),
               SizedBox(
                 height: width < 600 ? hight * .35 : width * .5,
                 child: const CustomBookImage(),
               ),
-              const SizedBox(height: 35),
+              const SizedBox(height: 20),
               const Text(
                 'The jungle Book ',
                 style: Styles.textStyle30,
               ),
-              const SizedBox(height: 3),
+              const SizedBox(height: 5),
               Text(
                 'Rudyard kipling',
                 style: Styles.textStyle18.copyWith(
                   fontStyle: FontStyle.italic,
                 ),
               ),
-              const SizedBox(height: 18),
+              const SizedBox(height: 10),
               const BookRating(),
-              const SizedBox(height: 40),
+              const SizedBox(height: 25),
               const BookActions(),
+              const SizedBox(height: 25),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also Like ',
+                  style: Styles.textStyle18.copyWith(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const SimiilarBooksListView(),
+              const SizedBox(height: 10),
             ],
           ),
         ),
---
+++ lib/features/book feature/presintation/view/widgets/simillar_books_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'custom_book_image.dart';
+class SimiilarBooksListView extends StatelessWidget {
+  const SimiilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double hight = MediaQuery.of(context).size.height;
+    return SizedBox(
+      height: width < 600 ? hight * .19 : width * .33,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return const CustomBookImage();
+        },
+      ),
+    );
+  }
+}

a4d285cb2752951259ee213f65322576e544a642
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-08-04 20:12:59 +0300

    implement fetch featured books

--- lib/features/book feature/data/repositry/book_repo_impl.dart
+++ lib/features/book feature/data/repositry/book_repo_impl.dart
@@ -32,7 +32,22 @@ class BookRepoImpl implements BookRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+      List<BookModel> books = [];
+      for (var book in data['items']) {
+        books.add(BookModel.fromJson(book));
+      }
+
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioExcepiton(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }

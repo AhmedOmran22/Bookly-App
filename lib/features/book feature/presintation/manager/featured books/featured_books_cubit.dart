0f3c29e95412d7a83627e55abd75765c8a94c552
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-08-04 20:35:06 +0300

    create fetch featured books method in cubit

--- lib/features/book feature/presintation/manager/featured books/featured_books_cubit.dart
+++ lib/features/book feature/presintation/manager/featured books/featured_books_cubit.dart
@@ -2,9 +2,27 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 import '../../../data/models/book model/book_model.dart';
+import '../../../data/repositry/book_repo_impl.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.bookRepoImpl) : super(FeaturedBooksInitial());
+
+  final BookRepoImpl bookRepoImpl;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    var result = await bookRepoImpl.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
